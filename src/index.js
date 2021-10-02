25f69b668500f6f3f496144cd3b100bf9c9ed3ca
Ruthenic <mdrakea3@tutanota.com>
2021-10-02 15:48:42 -0400

    do some stuff

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 // © 2021 Tightlinez, licensed under BSD-3-Clause
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './main.css';
 
 import * as bootstrap from 'bootstrap';
-
 let setupNavbar = async () => {
 	console.log(
 	    'If you\'re looking here, you should probably help me with the site.');
 	let headerContent = `
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
 		<div class="container-fluid">
-			<a class="navbar-brand" href="/#">Tightlinez</a>
+			<a class="navbar-brand" href="/#">
+				<!--<img src="/images/logo.png" width="60" height="60" class="d-inline-block align-top" />-->
+				Tightlinez
+			</a>
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
